bdcc36f39f205259d817332b4db96451fc7d23fd
vlemert <niels.vleeming@gmail.com>
2015-07-23 20:02:53 +0200

    Added sortable dependency, installed jspm dependencies.

---
+++ config.js
@@ -0,0 +1,16 @@
+System.config({
+  "transpiler": "traceur",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "sortable": "github:rubaxa/sortable@1.2.1",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
+  }
+});
+
